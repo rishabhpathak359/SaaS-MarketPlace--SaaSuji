16ae30b7b5493ccc4dcf8a60b55a7b85d538621f
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-07-29 08:11:18 +0530

    URL Fixed

--- Seller/src/constants.js
+++ Seller/src/constants.js
@@ -1,5 +1,5 @@
-const HOST_NAME = "http://localhost:8000/api/v1/"
+const HOST_NAME = "https://sassujibackend.onrender.com/api/v1/"
 // https://sassujibackend.onrender.com/api/v1/
 //http://localhost:8000/api/v1/
-const SERVER_URL = "http://localhost:8000/"
+const SERVER_URL = "https://sassujibackend.onrender.com/"
 export {HOST_NAME,SERVER_URL}
